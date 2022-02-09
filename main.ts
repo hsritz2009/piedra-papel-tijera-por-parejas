25505a6ef51eb28b2e9eb64827ae13e5362067e3
hsritz2009 <98743112+hsritz2009@users.noreply.github.com>
2022-02-09 09:30:09 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,86 @@
-let jugada = 0
-radio.setGroup(51)
-basic.forever(function () {
+input.onGesture(Gesture.Shake, function () {
     jugada = randint(1, 3)
+    radio.sendNumber(jugada)
     if (jugada == 1) {
         basic.showLeds(`
+            . . . . .
             # # # # #
-            # . . . #
-            # . . . #
-            # . . . #
             # # # # #
+            # # # # #
+            . . . . .
             `)
+        basic.pause(1500)
+        if (receivedString == 1) {
+            basic.showIcon(IconNames.Sad)
+            radio.sendNumber(4)
+        } else if (receivedString == 2) {
+            basic.showIcon(IconNames.Happy)
+            radio.sendNumber(5)
+        } else {
+            basic.showIcon(IconNames.No)
+            radio.sendNumber(6)
+        }
     } else if (jugada == 2) {
         basic.showLeds(`
-            # # . . .
-            # # # . #
-            . . . # .
-            # # # . #
-            # # . . .
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
             `)
-    } else {
+    } else if (jugada) {
         basic.showLeds(`
-            . . . . .
-            # # # # #
-            # # # # #
-            # # # # #
-            . . . . .
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
             `)
     }
 })
+radio.onReceivedString(function (receivedString) {
+    jugada23 = randint(1, 3)
+    if (jugada23) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            . . . . .
+            `)
+        basic.pause(1500)
+        if (receivedString == 1) {
+            basic.showIcon(IconNames.Happy)
+            radio.sendNumber(5)
+        } else if (receivedString == 2) {
+            basic.showIcon(IconNames.No)
+            radio.sendNumber(6)
+        } else {
+            basic.showIcon(IconNames.Sad)
+            radio.sendNumber(4)
+        }
+    } else if (jugada23 == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (jugada23) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
+let jugada23 = 0
+let jugada = 0
+radio.setGroup(51)
