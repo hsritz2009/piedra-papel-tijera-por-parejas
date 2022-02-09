6a868836f10d64f423f697f3482c71af9b4171b4
hsritz2009 <98743112+hsritz2009@users.noreply.github.com>
2022-02-09 08:50:14 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,30 @@
+let jugada = 0
+radio.setGroup(51)
 basic.forever(function () {
-	
+    jugada = randint(1, 3)
+    if (jugada == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (jugada == 2) {
+        basic.showLeds(`
+            # # . . .
+            # # # . #
+            . . . # .
+            # # # . #
+            # # . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            . . . . .
+            `)
+    }
 })
